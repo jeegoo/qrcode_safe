a7b42ed4e8d66beeea008a06d3b4288bc1146017
kebir <jugurtha.kebir.etu@univ-lille.fr>
2020-11-30 15:53:48 +0100

    refactor: changing the template to a react template

---
+++ src/layouts/HomePage.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import Login from '../views/auth/LoginView';
+import MainLayout from "./MainLayout";
+
+
+
+export default class CreateClient extends Component{
+
+
+        constructor(props){
+              super(props);
+              this.is_logged_in = this.is_logged_in.bind(this);
+              this.displayRightPage = this.displayRightPage.bind(this);
+
+        }
+
+        is_logged_in (){
+             return true;
+        }
+
+        displayRightPage(){
+
+              if(this.is_logged_in())
+                   return <MainLayout />;
+              return <Login />;
+
+
+        }
+
+
+        render() {
+                  return (
+                    <div className="login">
+                      {this.displayRightPage()}
+                    </div>
+                  );
+          }
+
+
+};
+
+
