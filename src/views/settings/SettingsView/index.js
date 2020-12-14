b7f65e4bc0f608715e85e59bf1ec30dbf9a30cd8
kebir <jugurtha.kebir.etu@univ-lille.fr>
2020-12-14 11:45:30 +0100

    debug: dedugging settings page

--- src/views/settings/SettingsView/index.js
+++ src/views/settings/SettingsView/index.js
@@ -1,72 +1,38 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Fade from '@material-ui/core/Fade';
-import Button from '@material-ui/core/Button';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Typography from '@material-ui/core/Typography';
+import {
+  Box,
+  Container,
+  makeStyles
+} from '@material-ui/core';
+import Page from 'src/components/Page';
+import Notifications from './Notifications';
+import Password from './Password';
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-  },
-  button: {
-    margin: theme.spacing(2),
-  },
-  placeholder: {
-    height: 40,
-  },
+    backgroundColor: theme.palette.background.dark,
+    minHeight: '100%',
+    paddingBottom: theme.spacing(3),
+    paddingTop: theme.spacing(3)
+  }
 }));
 
-export default function DelayingAppearance() {
+const SettingsView = () => {
   const classes = useStyles();
-  const [loading, setLoading] = React.useState(false);
-  const [query, setQuery] = React.useState('idle');
-  const timerRef = React.useRef();
-
-  React.useEffect(
-    () => () => {
-      clearTimeout(timerRef.current);
-    },
-    [],
-  );
-
-  const handleClickLoading = () => {
-    setLoading((prevLoading) => !prevLoading);
-  };
-
-  const handleClickQuery = () => {
-    clearTimeout(timerRef.current);
-
-    if (query !== 'idle') {
-      setQuery('idle');
-      return;
-    }
-
-    setQuery('progress');
-    timerRef.current = window.setTimeout(() => {
-      setQuery('success');
-    }, 2000);
-  };
 
   return (
-    <div className={classes.root}>
-      <div className={classes.placeholder}>
-        <Fade
-          in={loading}
-          style={{
-            transitionDelay: loading ? '800ms' : '0ms',
-          }}
-          unmountOnExit
-        >
-          <CircularProgress />
-        </Fade>
-      </div>
-      <Button onClick={handleClickLoading} className={classes.button}>
-        {loading ? 'Stop loading' : 'Loading'}
-      </Button>
-
-    </div>
+    <Page
+      className={classes.root}
+      title="Settings"
+    >
+      <Container maxWidth="lg">
+        <Notifications />
+        <Box mt={3}>
+          <Password />
+        </Box>
+      </Container>
+    </Page>
   );
-}
+};
+
+export default SettingsView;
